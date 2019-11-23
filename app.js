845c12cc0db465aa981b4ea2e08badc94ba3cc43
Fisayo <aikomofisayo@gmail.com>
2019-11-23 13:28:54 +0100

    Set up ESLint

--- app.js
+++ app.js
@@ -5,14 +5,14 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 app.get('/', (req, res) => {
-    res.send('Nothing to do here!')
+  res.send('Nothing to do here!');
 });
 
 app.use('/api/v1/', (req, res) => {
-    res.json({
-        status: 'success',
-        message: 'Welcome to TeamWork API v1'
-    })
+  res.json({
+    status: 'success',
+    message: 'Welcome to TeamWork API v1',
+  });
 });
 
 module.exports = app;
--- test/app.test.js
+++ test/app.test.js
@@ -1,18 +1,20 @@
-const app = require('../app');
 const chai = require('chai');
 const chaiHttp = require('chai-http');
+const app = require('../app');
 
 const { expect } = chai;
 chai.use(chaiHttp);
 
 describe('The Server', () => {
-    it('Welcomes user to the api', (done) => {
-        chai.request(app).get('/api/v1/')
-            .end((err, res) => {
-                expect(res).to.have.status(200);
-                expect(res.body.status).to.equal('success');
-                expect(res.body.message).to.equal('Welcome to TeamWork API v1');
-                done();
-            });
-    });
+  it('Welcomes user to the api', (done) => {
+    chai
+      .request(app)
+      .get('/api/v1/')
+      .end((err, res) => {
+        expect(res).to.have.status(200);
+        expect(res.body.status).to.equal('success');
+        expect(res.body.message).to.equal('Welcome to TeamWork API v1');
+        done();
+      });
+  });
 });
