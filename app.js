209ee099350bb2da19b3d55c470aaea76ef1ff26
Fisayo <aikomofisayo@gmail.com>
2019-11-26 00:29:40 +0100

    Add get feed feature

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const employeeRoutes = require('./routes/employees');
 const gifRoutes = require('./routes/gifs');
 const articleRoutes = require('./routes/articles');
+const feedRoutes = require('./routes/feeds');
 
 const app = express();
 app.use(express.json());
@@ -18,6 +19,7 @@ app.get("/api/v1/", (req, res) => {
   });
 });
 
+app.use('/api/v1/feed', feedRoutes);
 app.use('/api/v1/auth', employeeRoutes);
 app.use('/api/v1/gifs', gifRoutes);
 app.use('/api/v1/articles', articleRoutes)
---
+++ routes/feeds.js
@@ -0,0 +1,9 @@
+const express = require("express");
+const router = express.Router();
+const auth = require('../middleware/auth');
+
+const feedCtrl = require('../controllers/feeds');
+
+router.get('/', auth, feedCtrl.getFeed);
+
+module.exports = router;
