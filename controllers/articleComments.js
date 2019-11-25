366fd2bb41b94a0b310a216a9f3d3d6a9a486165
Fisayo <aikomofisayo@gmail.com>
2019-11-25 22:53:16 +0100

    Add article comment feature

---
+++ controllers/articleComments.js
@@ -0,0 +1,43 @@
+const moment = require("moment");
+const uuidv4 = require("uuid/v4");
+const db = require("../db/index");
+
+const Comment = {
+  async create(req, res) {
+    const text = `INSERT INTO "public"."tw_article_comments" 
+        (id, comment, owner_id, article_id, created_on, modified_on) 
+        VALUES ($1, $2, $3, $4, $5, $6) returning *`;
+
+    const getArticlePropQuery = `SELECT * FROM "public"."tw_articles" WHERE id=$1`;
+
+    const createdOn = moment(new Date());
+
+    try {
+      const response = await db.query(getArticlePropQuery, [req.params.articleId]);
+      const values = [
+        uuidv4(),
+        req.body.comment,
+        req.user.id,
+        req.params.articleId,
+        createdOn,
+        moment(new Date())
+      ];
+      const { rows } = await db.query(text, values);
+      return res.status(201).send({
+        status: "success",
+        data: {
+          message: "Comment successfully created",
+          createdOn,
+          articleTitle: response.rows[0].title,
+          article: response.rows[0].article_body,
+          comment: req.body.comment
+        }
+      });
+    } catch (error) {
+        console.log(error);
+      return res.status(400).send(error);
+    }
+  }
+};
+
+module.exports = Comment;
--- routes/articles.js
+++ routes/articles.js
@@ -3,9 +3,11 @@ const router = express.Router();
 const auth = require('../middleware/auth');
 
 const articleCtrl = require('../controllers/articles');
+const articleCommentCtrl = require('../controllers/articleComments');
 
 router.post('/', auth, articleCtrl.create);
 router.put('/:articleId', auth, articleCtrl.update)
+router.post('/:articleId/comment', auth, articleCommentCtrl.create);
 router.delete('/:articleId', auth, articleCtrl.delete);
 
 module.exports = router;
